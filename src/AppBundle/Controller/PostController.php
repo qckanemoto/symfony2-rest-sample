55c15d53224f8083952f3b5f2a0e2c7e64f2cae4
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-05-13 09:57:56 +0900

    6. ルーティングを設定

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -3,7 +3,28 @@
 namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use Symfony\Component\HttpFoundation\Request;
 
-class PostController extends FOSRestController
+class PostController extends FOSRestController implements ClassResourceInterface
 {
+    public function cgetAction()
+    {
+    }
+
+    public function getAction($id)
+    {
+    }
+
+    public function postAction(Request $request)
+    {
+    }
+
+    public function getCommentsAction($id)
+    {
+    }
+
+    public function postCommentAction($id, Request $request)
+    {
+    }
 }
