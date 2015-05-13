eadf55ee945763fa878a62dec09a7f41a97946d6
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-05-13 10:08:00 +0900

    9. post 系アクションを実装

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -2,6 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Comment;
+use AppBundle\Entity\Post;
+use AppBundle\Form\CommentType;
+use AppBundle\Form\PostType;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,6 +28,21 @@ class PostController extends FOSRestController implements ClassResourceInterface
 
     public function postAction(Request $request)
     {
+        $form = $this->get('form.factory')->createNamed('', new PostType(), $post = new Post(), [
+            'csrf_protection' => false,
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $post;
+        }
+
+        return $form;
     }
 
     public function getCommentsAction($id)
@@ -35,6 +54,24 @@ class PostController extends FOSRestController implements ClassResourceInterface
 
     public function postCommentAction($id, Request $request)
     {
+        $comment = new Comment();
+        $comment->setPost($this->getRepository()->find($id));
+
+        $form = $this->get('form.factory')->createNamed('', new CommentType(), $comment, [
+            'csrf_protection' => false,
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            return $comment;
+        }
+
+        return $form;
     }
 
     private function getRepository()
